457e78c060c8e360983024bd7dcc17e33f7ae8c2
Paulo Henrique <paulo-henriquegt@hotmail.com>
2021-05-08 16:57:08 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,12 +1,19 @@
 import Chart from 'react-apexcharts';
 
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
+
 const DonutChart = () => {
+
+    let chartData: ChartData = { labels: [], series: [] };
 
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+
     const options = {
         legend: {
             show: true
